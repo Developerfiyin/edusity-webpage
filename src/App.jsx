3868aea1ba2a8ee86e2efea088861248e63df337
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-09-18 12:34:16 +0100

    updated programs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Hero from "./Components/Hero/Hero";
 import Navbar from "./Components/Navbar/Navbar";
+import Programs from "./Components/Programs/programs";
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Programs />
     </div>
   );
 };
--- src/Components/Programs/programs.jsx
+++ src/Components/Programs/programs.jsx
@@ -1,5 +1,16 @@
 import "./programs.css";
+import Program1 from "../../assets/program-1.png";
+import Program2 from "../../assets/program-2.png";
+import Program3 from "../../assets/program-3.png";
 const Programs = () => {
-  return <div className="programs"></div>;
+  return (
+    <div className="programs">
+      <div className="program ">
+        <img src={Program1} alt="The program image" />
+        <img src={Program2} alt="The program image 2" />
+        <img src={Program3} alt="The program image 3" />
+      </div>
+    </div>
+  );
 };
 export default Programs;
