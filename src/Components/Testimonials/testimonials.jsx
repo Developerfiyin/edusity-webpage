ab05febc1e9585d64af0dfbba69daf604430f6a5
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-09-19 14:06:13 +0100

    styled div slider

--- src/Components/Testimonials/testimonials.jsx
+++ src/Components/Testimonials/testimonials.jsx
@@ -30,6 +30,42 @@ const Testimonials = () => {
               </p>
             </div>
           </li>
+
+          <li>
+            <div className="slider">
+              <div className="user-info">
+                <img src={user2} alt="The user 2 image" />
+                <div>
+                  <h3>William Jackson</h3>
+                  <span>Edusity, USA</span>
+                </div>
+              </div>
+              <p>
+                Choosing to pursue my degree at Edusity was one of the best
+                decisions I've ever made. The supportive community,
+                state-of-the-art facilities, and commitment to academic
+                excellence have truly exceeded my expectation.
+              </p>
+            </div>
+          </li>
+
+          <li>
+            <div className="slider">
+              <div className="user-info">
+                <img src={user1} alt="The user 1 image" />
+                <div>
+                  <h3>William Jackson</h3>
+                  <span>Edusity, USA</span>
+                </div>
+              </div>
+              <p>
+                Choosing to pursue my degree at Edusity was one of the best
+                decisions I've ever made. The supportive community,
+                state-of-the-art facilities, and commitment to academic
+                excellence have truly exceeded my expectation.
+              </p>
+            </div>
+          </li>
         </ul>
 
         <img src={user2} alt="The user 2 image " />
