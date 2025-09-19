f4c96933161cd98bd09f12c6039357d544e05516
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-09-19 19:01:34 +0100

    updated testimonials

--- src/Components/Testimonials/testimonials.jsx
+++ src/Components/Testimonials/testimonials.jsx
@@ -5,14 +5,40 @@ import user1 from "../../assets/user-1.png";
 import user2 from "../../assets/user-2.png";
 import user3 from "../../assets/user-3.png";
 import user4 from "../../assets/user-4.png";
+import { useRef } from "react";
 
 const Testimonials = () => {
+  const slider = useRef();
+  let tx = 0;
+  const slideForward = () => {
+    if (tx > -50) {
+      tx -= 25;
+    }
+    slider.current.style.transform = `translateX(${tx}%)`;
+  };
+
+  const slideBackward = () => {
+    if (tx > 0) {
+      tx += 25;
+    }
+    slider.current.style.transform = `translateX(${tx}%)`;
+  };
   return (
     <div className="testimonials">
-      <img src={nexticon} alt="The back icon" className="next-btn" />
-      <img src={backicon} alt="the next icon" className="back-icon" />
+      <img
+        src={nexticon}
+        alt="The back icon"
+        className="next-btn"
+        onClick={slideForward}
+      />
+      <img
+        src={backicon}
+        alt="the next icon"
+        className="back-icon"
+        onClick={slideBackward}
+      />
       <div className="slider">
-        <ul>
+        <ul ref={slider}>
           <li>
             <div className="slide">
               <div className="user-info">
@@ -35,6 +61,24 @@ const Testimonials = () => {
             <div className="slide">
               <div className="user-info">
                 <img src={user2} alt="The user 2 image" />
+                <div>
+                  <h3>William Jackson</h3>
+                  <span>Edusity, USA</span>
+                </div>
+              </div>
+              <p>
+                Choosing to pursue my degree at Edusity was one of the best
+                decisions I've ever made. The supportive community,
+                state-of-the-art facilities, and commitment to academic
+                excellence have truly exceeded my expectation.
+              </p>
+            </div>
+          </li>
+
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user1} alt="The user 1 image" />
                 <div>
                   <h3>William Jackson</h3>
                   <span>Edusity, USA</span>
