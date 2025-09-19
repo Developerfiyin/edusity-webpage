0be629844d81245c3b5493945f268a46bed45686
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-09-19 13:41:27 +0100

    created a div slider

--- src/Components/Testimonials/testimonials.jsx
+++ src/Components/Testimonials/testimonials.jsx
@@ -11,8 +11,27 @@ const Testimonials = () => {
     <div className="testimonials">
       <img src={nexticon} alt="The back icon" className="next-btn" />
       <img src={backicon} alt="the next icon" className="back-icon" />
-      <div className="img-slider">
-        <img src={user1} alt="The user 1 image" />
+      <div className="slider">
+        <ul>
+          <li>
+            <div className="slider">
+              <div className="user-info">
+                <img src={user1} alt="The user 1 image" />
+                <div>
+                  <h3>William Jackson</h3>
+                  <span>Edusity, USA</span>
+                </div>
+              </div>
+              <p>
+                Choosing to pursue my degree at Edusity was one of the best
+                decisions I've ever made. The supportive community,
+                state-of-the-art facilities, and commitment to academic
+                excellence have truly exceeded my expectation.
+              </p>
+            </div>
+          </li>
+        </ul>
+
         <img src={user2} alt="The user 2 image " />
         <img src={user3} alt="The user 3 image" />
         <img src={user4} alt="The user 4 image" />
