12a761723be56168654b40c5ff6440ce3af3a9cd
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-09-19 11:31:41 +0100

    Updated campus folder

--- src/Components/Campus/campus..jsx
+++ src/Components/Campus/campus..jsx
@@ -3,6 +3,7 @@ import gallery1 from "../../assets/gallery-1.png";
 import gallery2 from "../../assets/gallery-2.png";
 import gallery3 from "../../assets/gallery-3.png";
 import gallery4 from "../../assets/gallery-4.png";
+import whitearrow from "../../assets/white-arrow.png";
 
 const Campus = () => {
   return (
@@ -10,9 +11,12 @@ const Campus = () => {
       <div className="gallery">
         <img src={gallery1} alt="the first gallery image" />
         <img src={gallery2} alt="The second gallery image" />
-        <img src={gallery3} alt="The third gallery iamge" />
+        <img src={gallery3} alt="The third gallery image" />
         <img src={gallery4} alt="The fourth gallery image" />
       </div>
+      <button className="btn dark-icon">
+        See more <img src={whitearrow} alt="the white arrow image" />
+      </button>
     </div>
   );
 };
