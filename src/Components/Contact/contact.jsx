faa9487a7857f99575e8211aff29226c961e4786
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-09-23 15:07:27 +0100

    Added span to the form

--- src/Components/Contact/contact.jsx
+++ src/Components/Contact/contact.jsx
@@ -4,8 +4,32 @@ import mailicon from "../../assets/mail-icon.png";
 import phoneicon from "../../assets/phone-icon.png";
 import locationicon from "../../assets/location-icon.png";
 import arrwwhite from "../../assets/white-arrow.png";
-
+import React from "react";
 const Contact = () => {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "YOUR_ACCESS_KEY_HERE");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
   return (
     <div className="contact">
       <div className="contact-col">
@@ -38,7 +62,7 @@ const Contact = () => {
         </ul>
       </div>
       <div className="contact-col">
-        <form>
+        <form onSubmit={onSubmit}>
           <label htmlFor="Name"> YOUR NAME :</label> <br />
           <input
             type="text"
@@ -65,6 +89,7 @@ const Contact = () => {
             Submit now <img src={arrwwhite} alt="the white arrow icon" />
           </button>
         </form>
+        <span>{result}</span>
       </div>
     </div>
   );
