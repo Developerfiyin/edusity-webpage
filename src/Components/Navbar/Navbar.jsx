8fbc0b878bbe22ebd5ab435374aa260f09f17030
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-09-26 10:49:28 +0100

    made the scroll link active

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,18 @@
 import { useEffect, useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
+import {
+  Link,
+  Button,
+  Element,
+  Events,
+  animateScroll as scroll,
+  scrollSpy,
+  scroller,
+} from "react-scroll";
+import * as Scroll from "react-scroll";
+
+//let Link = Scroll.Link;
 
 const Navbar = () => {
   const [sticky, setSticky] = useState(false);
@@ -15,12 +27,39 @@ const Navbar = () => {
       <nav className={`container ${sticky ? "dark-nav" : ""}`}>
         <img src={logo} alt="logo image" className="logo" />
         <ul>
-          <li>Home</li>
-          <li>Program</li>
-          <li>Testimonials</li>
-          <li>About us</li>
           <li>
-            <button className="btn"> Contact us</button>
+            <Link to="hero" smooth={true} offset={0} duration={500}>
+              Home
+            </Link>
+          </li>
+
+          <li>
+            {" "}
+            <Link to="programs" smooth={true} offset={0} duration={500}>
+              {" "}
+              Program
+            </Link>{" "}
+          </li>
+
+          <li>
+            {" "}
+            <Link to="testimonials" smooth={true}>
+              Testimonials
+            </Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="about" smooth={true} duration={500} offset={0}>
+              About us
+            </Link>
+          </li>
+          <li>
+            <button className="btn">
+              {" "}
+              <Link to="contact" duration={500} offset={0}>
+                Contact us
+              </Link>
+            </button>
           </li>
         </ul>
       </nav>
